2fd25c5c9c0efa048e7abdafabcd1a223a6e3179
Anna Lam <annalam91@gmail.com>
2021-02-16 17:20:32 +0000

    redoing the login screen again

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet} from 'react-native';
-import SignIn from './src/components/SignIn';
+import SignIn from './src/components/SignIn'
 
 export default function App() {
 
@@ -14,7 +14,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,67 +1,82 @@
 import React, {useState} from "react";
-import { Link } from "@reach/router";
+import {Link} from "@reach/router";
+import {
+    TouchableOpacity,
+    View,
+    Text,
+    TextInput,
+    StyleSheet,
+    KeyboardAvoidingView,
+    TouchableWithoutFeedback,
+    Keyboard
+} from 'react-native';
 import "./css/SignIn.css";
 
 const SignIn = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState(null);
-    const signInWithEmailAndPasswordHandler = 
-            (event,email, password) => {
-                event.preventDefault();
+    const signInWithEmailAndPasswordHandler = (event, email, password) => {
+        event.preventDefault();
     };
 
-      const onChangeHandler = (event) => {
-          const {name, value} = event.currentTarget;
+    const onChangeHandler = (event) => {
+        const {name, value} = event.currentTarget;
 
-          if(name === 'userEmail') {
-              setEmail(value);
-          }
-          else if(name === 'userPassword'){
+        if (name === 'userEmail') {
+            setEmail(value);
+        } else if (name === 'userPassword') {
             setPassword(value);
-          }
-      };
+        }
+    };
 
-  return (
-    <div>
-      <h1>Welcome</h1>
-      <div>
-        {error !== null && <div>{error}</div>}
-        <form className="email-form">
-          <input
-            label="Email:"
-            type="email"
-            name="userEmail"
-            value = {email}
-            placeholder="Email"
-            id="userEmail"
-            onChange = {(event) => onChangeHandler(event)}
-          />
-          <input 
-            type="password"
-            name="userPassword"
-            value = {password}
-            placeholder="Password"
-            id="userPassword"
-            onChange = {(event) => onChangeHandler(event)}
-          />
-          <button onClick = {(event) => {signInWithEmailAndPasswordHandler(event, email, password)}}>
-            Sign in
-          </button>
-        </form>
-        <p className="text-center my-3">or</p>
-        <p className="text-center my-3">
-          Don't have an account?{" "}
-          <Link to="signUp" className="text-blue-500 hover:text-blue-600">
-            Sign up here
-          </Link>{" "}
-          <br />{" "}
-          <Link to = "passwordReset" className="text-blue-500 hover:text-blue-600">
-            Forgot Password?
-          </Link>
-        </p>
-      </div>
-    </div>
-  );
+    return (
+        <div> {
+            error !== null && <div>{error}</div>
+        }
+        <View style={styles.containerView}>
+            <Text style={styles.loginText}>Login</Text>
+                <TextInput style={styles.emailTextInput} value={"Email"}/>
+                <TextInput style={styles.passwordTextInput} value={"Passwrod"}/>
+                <TouchableOpacity style={styles.loginButton}>
+                    <Text>Login</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.loginButton}>
+                    <Text>Sign Up</Text>
+                </TouchableOpacity>
+        </View>
+          
+        </div>
+    );
 };
 export default SignIn;
+
+const styles = StyleSheet.create({
+    containerView: {
+        flex: 1,
+    },
+    loginText:{
+        paddingTop: 100,
+        textAlign: 'center',
+        fontSize: 18,
+        fontWeight: 600
+    },
+    emailTextInput: {
+        marginTop: 150,
+        height: 40,
+        borderBottomColor: 'gray',
+        borderBottomWidth: 3
+    },
+    passwordTextInput: {
+        marginTop: 3,
+        height: 40,
+        borderBottomColor: 'gray',
+        borderBottomWidth: 1
+    },
+    loginButton: {
+        alignItems: "center",
+        backgroundColor: "#DDDDDD",
+        padding: 10
+      },
+
+});
